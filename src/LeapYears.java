1013bc09220750a8f62f73016400c66b9cbc144a
Rand0MTutoriaL <thanwisitang7910@gmail.com>
2025-09-08 15:49:07 +0700

    update wrong code in 8.1

--- src/LeapYears.java
+++ src/LeapYears.java
@@ -1,6 +1,6 @@
 public class LeapYears {
     public static void main(String[] args) {
-        int count = 1;
+        int count = 0;
         for (int year = 101; year <= 2100; year++) {
             if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
                 if (count % 10 == 0) System.out.println(year);
